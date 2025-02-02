f524b933a667594e765ca5a2ba045eb100702ee4
Zaid0412 <108808139+Zaid0412@users.noreply.github.com>
2025-02-02 17:32:53 +0300

    Switch db back to railway db

--- db/pool.js
+++ db/pool.js
@@ -3,14 +3,14 @@ require('dotenv').config()
 
 const connectionString = 'postgresql://postgres:tQmcZDIIiUWpSVqWWhJTSxSfvNDRyqVe@viaduct.proxy.rlwy.net:31127/railway'
 
-module.exports = new Pool({
-    host:process.env.HOST,
-    user:process.env.USER,
-    database:process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: process.env.DATABASE_PORT
-})
-
 // module.exports = new Pool({
-//     connectionString,
-// });
+//     host:process.env.HOST,
+//     user:process.env.USER,
+//     database:process.env.DATABASE,
+//     password: process.env.PASSWORD,
+//     port: process.env.DATABASE_PORT
+// })
+
+module.exports = new Pool({
+    connectionString,
+});
